affbc454f6b0ff03fa90e4279ce42d50b51ce374
Kunal Bhadana <130590476+Kunal-5020@users.noreply.github.com>
2026-02-04 18:07:34 +0530

    Update route.js

--- src/app/.well-known/assetlinks.json/route.js
+++ src/app/.well-known/assetlinks.json/route.js
@@ -23,6 +23,16 @@ export async function GET() {
           "FA:C6:17:45:DC:09:03:78:6F:B9:ED:E6:2A:96:2B:39:9F:73:48:F0:BB:6F:89:9B:83:32:66:75:91:03:3B:9C" 
         ]
       }
+    },
+    {
+      "relation": ["delegate_permission/common.handle_all_urls"],
+      "target": {
+        "namespace": "android_app",
+        "package_name": "com.vaidiktalk",
+        "sha256_cert_fingerprints": [
+          "D2:69:B1:2A:40:23:DF:AD:C0:F5:D0:55:E2:75:B6:EB:EA:F5:FB:BB:75:1F:B4:5C:A1:64:A9:25:FD:14:2E:85" 
+        ]
+      }
     }
   ];
 
